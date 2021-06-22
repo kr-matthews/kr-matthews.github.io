6191b320d732f7ba6a3af953b9f58d222ae56201
Kevin Matthews <kvnrmatthews@gmail.com>
2021-06-22 14:11:12 -0700

    Minor rephrasing.

--- src/Notes.js
+++ src/Notes.js
@@ -4,7 +4,7 @@ function About(props) {
   return (
     <div className="page narrow-page">
       <h1>Notes</h1>
-      <p>Nothing here yet.</p>
+      <p>Nothing here so far.</p>
     </div>
   )
 }
