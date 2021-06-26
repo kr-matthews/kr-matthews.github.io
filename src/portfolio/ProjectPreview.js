2ca2a4cf397eb7955ac50d4ac4e484250b309722
Kevin Matthews <kvnrmatthews@gmail.com>
2021-06-26 09:11:20 -0700

    Cleaned up project list data, improved display (added tags).

--- src/portfolio/ProjectPreview.js
+++ src/portfolio/ProjectPreview.js
@@ -2,7 +2,7 @@
 import {Link} from 'react-router-dom';
 */
 
-function Project({title, url, image, category, languages, year, description}) {
+function Project({title, url, image, tags, languages, year, description}) {
   return (
         <a href={url} className="item" target="_blank" rel="noopener noreferrer">
           <div className="image-container">
@@ -10,7 +10,10 @@ function Project({title, url, image, category, languages, year, description}) {
           </div>
           <div className="info-container">
             <h2>{title}</h2>
-            <div className="specs">{languages ? year + " | " + languages : year}</div>
+            <div className="specs">
+              {languages.length ? year + " | " + languages.join(", ") : year}
+            </div>
+            {tags.length && <div className="specs">{tags.join(", ")}</div>}
             <div className="description">{description}</div>
           </div>
         </a>
