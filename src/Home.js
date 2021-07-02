bf0553b67ee6d157c96f2cba9903435ef46db5ca
Kevin Matthews <kvnrmatthews@gmail.com>
2021-07-02 15:35:22 -0700

    Change description from active to past project on home page.

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ function Home(props) {
   return (
     <div className="page narrow-page">
       <h1>Kevin's Website</h1>
-      <p>This is a project I'm working on, with the primary aim of becoming
+      <p>This is a project I was working on, with the primary aim of becoming
       more familiar with websites, and in particular, React and CSS.</p>
       <p>Pick something to visit in the top navigation bar. If you want.</p>
     </div>
