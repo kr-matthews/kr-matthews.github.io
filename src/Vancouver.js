f4b7cdd51b344e18fb756fdf31061a52d52391a7
Kevin Matthews <kvnrmatthews@gmail.com>
2021-10-10 16:17:24 -0700

    Add to Vancouver.

--- src/Vancouver.js
+++ src/Vancouver.js
@@ -14,15 +14,43 @@ function Vancouver(props) {
         </a>
         . I've highlight many of them in the map below. Vancouver has also been
         expanding and improving bike infrastructure over the past few decades,
-        which I've additionally highlighted in the map below. I'll be adding to
-        this map as I find more things.
+        which I've highlighted too. I'll be adding to this map as I find more
+        things.
       </p>
+      <h2>Custom Map</h2>
       <iframe
         title="vancouverMap"
         src="https://www.google.com/maps/d/u/0/embed?mid=1VjjBQZ-70CWIjFpoo8BHS_bLgm3xpQQ3"
         width="750"
-        height="480"
+        height="550"
       ></iframe>
+      <h2>Street Name Resources</h2>
+      Here are some links with more information:
+      {/* // TODO: fix css for list/list items */}
+      <ul>
+        <li>
+          a{" "}
+          <a
+            href="https://geographic.org/streetview/canada/bc/vancouver.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            list
+          </a>{" "}
+          of all Vancouver streets
+        </li>
+        <li>
+          an{" "}
+          <a
+            href="https://newsinteractives.cbc.ca/longform/streets"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            article
+          </a>{" "}
+          from the CBC examining the origins of all Vancouver streets
+        </li>
+      </ul>
     </div>
   );
 }
