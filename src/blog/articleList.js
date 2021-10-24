c2e6cda06053393987d57233e9c48ab3d417c5c8
Kevin Matthews <kvnrmatthews@gmail.com>
2021-10-24 16:22:03 -0700

    Publish second blog post.
    002 - React State Management
    Including react logo as preview picture.

--- src/blog/articleList.js
+++ src/blog/articleList.js
@@ -1,10 +1,12 @@
 // import artcle contents
 
 import RubiksCubeBlindfolded from "./articles/001-rubiks-cube-blindfolded/rubiks-cube-blindfolded.js";
+import ReactStateManagement from "./articles/002-react-state-management/react-state-management.js";
 
 // import article preview images
 
 import altSolvedCube from "./articles/001-rubiks-cube-blindfolded/alt-solved-cube-physical.jpg";
+import reactLogo from "./articles/002-react-state-management/react-logo.png";
 
 /* this will be sorted by publishDate, so can be in any order */
 export const articles = [
@@ -21,14 +23,27 @@ export const articles = [
       "This is the first in a series of articles about solving Rubik's Cubes blindfolded. There are no prerequisites — you don't need to know how to solve a Rubik's Cube, or anything about them really.",
     content: <RubiksCubeBlindfolded />,
   },
+  {
+    title: "State Management in React",
+    link: "react-state-management",
+    startDate: new Date("2021-10-09"),
+    publishDate: new Date("2021-10-24"),
+    editDate: "",
+    image: reactLogo,
+    tags: ["Programming", "React"],
+    preview:
+      "Explaining the problems I encountered with state management while creating the game Connect 4 in React, the solutions I came up with, and why they usually were not good solutions. Not a tutorial or guide.",
+    content: <ReactStateManagement />,
+  },
   /*
     title: "",
     link: "",
     startDate: new Date(''),
     publishDate: new Date(''),
-    editDate: new Date(''),
+    editDate: "",
     image: "",
     tags: [],
     preview: ""
+    content: </>
   */
 ];
