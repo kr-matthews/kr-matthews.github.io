37d356e3440879ec08aaf007ada8c2ba6acdff17
Kevin Matthews <kvnrmatthews@gmail.com>
2023-01-29 14:37:03 -0800

    Remove hitwebcounter

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -11,21 +11,6 @@ export default function Home() {
         up based on everything I'd learnt.
       </p>
       <p>Pick something to visit in the top navigation bar. If you want.</p>
-      <p style={{ fontSize: "smaller" }}>
-        Unique website visits: {/* <!-- hitwebcounter Code START --> */}
-        <a
-          href="https://www.hitwebcounter.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src="https://hitwebcounter.com/counter/counter.php?page=7856224&style=0007&nbdigits=5&type=ip&initCount=0"
-            title="Free Counter"
-            alt="web counter"
-            border="0"
-          />
-        </a>
-      </p>
     </div>
   );
 }
