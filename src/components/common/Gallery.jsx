a47aa56555c7d6664af3f0f3750634f6493bddc4
Kevin Matthews <kvnrmatthews@gmail.com>
2023-02-12 21:11:40 -0800

    Add reusable Gallery components

---
+++ src/components/common/Gallery.jsx
@@ -0,0 +1,27 @@
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import React from "react";
+
+export default function Gallery({
+  childW = "400px",
+  children: tiles,
+  ...props
+}) {
+  return (
+    <SimpleGrid
+      templateColumns={`repeat(auto-fill, ${childW})`}
+      justifyContent="center"
+      gap="2em"
+      {...props}
+    >
+      {tiles}
+    </SimpleGrid>
+  );
+}
+
+export function GalleryItem({ w = "400px", children: content, ...props }) {
+  return (
+    <Box alignItems="center" w={w} textAlign="center" {...props}>
+      {content}
+    </Box>
+  );
+}
