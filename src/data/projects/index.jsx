8e332521a661a5296ee82636eba1503ec742caec
Kevin Matthews <kvnrmatthews@gmail.com>
2023-02-19 17:32:24 -0800

    Partially update projects data
    Need to add some icons still.

--- src/data/projects/index.jsx
+++ src/data/projects/index.jsx
@@ -1,36 +1,36 @@
-//// project images
+//// project images & icons
 
 import bldMemoToolImage from "./assets/bld-memo-tool.png";
 import bldMemoToolsImage from "./assets/bld-memo-tools.png";
 import threeStyleCornerAlgGeneratorImage from "./assets/three-style-corners-alg-generator.png";
 import cornerTwistAlgGeneratorImage from "./assets/corner-twist-alg-generator.png";
 import blindfoldedSystemImage from "./assets/blindfolded-system.png";
-import theUltimatePuzzleSolverImage from "./assets/the-ultimate-puzzle-solver.png";
-import heartsImage from "./assets/hearts-cards.jpg";
+import theUltimatePuzzleSolverImage from "./assets/the-ultimate-puzzle-pieces.png";
+import heartsImage from "./assets/hearts-console.png";
 import weakSwapImage from "./assets/weak-swap.png";
+import personalWebsiteImage from "./assets/website.png";
 import ticTacToeImage from "./assets/tic-tac-toe.png";
 import minesweeperImage from "./assets/minesweeper.png";
 import connectFourImage from "./assets/connect-four.png";
 import cribbageImage from "./assets/cribbage.png";
 import stageGroupsImage from "./assets/stage-groups.png";
-import activitiesOnThisDayImage from "./assets/calendar-activity.svg";
+import activitiesOnThisDayImage from "./assets/activities-on-this-day.png";
 import footerImage from "./assets/footer-example.png";
 
-// website logo used directly
-import personalWebsiteImage from "../../assets/logo.svg";
+// import theUltimatePuzzleSolverIcon from "./assets/the-ultimate-puzzle-pieces.png";
+import heartsIcon from "./assets/heart-card.svg";
+import personalWebsiteIcon from "../../assets/logo.svg";
+import activitiesOnThisDayIcon from "./assets/calendar-activity.svg";
 
-// todo: move wip to component?
-// import wip from "../../assets/wip.png";
+//// project list
 
-//// list
-
-// this will be sorted by id, so can be in any order
 export const projects = [
   {
+    id: 0,
     title: "BLD Memo Tool",
-    id: 201501,
-    url: "https://csclub.uwaterloo.ca/~krmatthe/BLD-Memo-Tool.cgi",
     image: bldMemoToolImage,
+    type: "Personal",
+    url: "https://csclub.uwaterloo.ca/~krmatthe/BLD-Memo-Tool.cgi",
     tags: ["Web App", "Rubik's Cubes"],
     languages: ["Racket"],
     year: 2015,
@@ -38,10 +38,11 @@ export const projects = [
       "Produces or checks memorization sequence for blindfolded solving of a scrambled Rubik's cube, given custom user settings.",
   },
   {
+    id: 1,
     title: "BLD Memo Tools",
-    id: 201502,
-    url: "https://csclub.uwaterloo.ca/~krmatthe/BLD-Memo-Tools.cgi",
     image: bldMemoToolsImage,
+    type: "Personal",
+    url: "https://csclub.uwaterloo.ca/~krmatthe/BLD-Memo-Tools.cgi",
     tags: ["Web App", "Rubik's Cubes"],
     languages: ["Racket"],
     year: 2015,
@@ -49,10 +50,12 @@ export const projects = [
       "Major expansion of prior project to include more puzzle sizes, visual display of cube state, new training tool, and much more.",
   },
   {
+    id: 2,
     title: "Weak Swap (Method)",
-    id: 201801,
-    url: "https://github.com/kr-matthews/weak-swap",
     image: weakSwapImage,
+    type: "Personal",
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/weak-swap",
     tags: ["Method", "Rubik's Cubes"],
     languages: [],
     year: 2018,
@@ -60,10 +63,13 @@ export const projects = [
       "An optimization for dealing with cycle breaks in blindfolded solving of Rubik's cubes, applicable when both memorizing and solving edges first. Joint work with Graham Siggins.",
   },
   {
+    id: 3,
     title: '"The Ultimate Puzzle" Solver',
-    id: 201901,
-    url: "https://github.com/kr-matthews/the-ultimate-puzzle-solver",
-    image: theUltimatePuzzleSolverImage, // consider replacing picture
+    image: theUltimatePuzzleSolverImage,
+    type: "Personal",
+    // icon: theUltimatePuzzleSolverIcon, // !!!
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/the-ultimate-puzzle-solver",
     tags: ["Puzzle", "Puzzle Solver"],
     languages: ["Racket"],
     year: 2019,
@@ -71,10 +77,13 @@ export const projects = [
       "A simple back-tracking program to find all solutions to a jigsaw-like puzzle. Can allow or disallow flipping pieces over from their natural face-up orientation.",
   },
   {
+    id: 4,
     title: "Hearts",
-    id: 201902,
-    url: "https://github.com/kr-matthews/hearts-java-v2",
     image: heartsImage,
+    type: "Personal",
+    icon: heartsIcon,
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/hearts-java-v2",
     tags: ["Game", "Cards"],
     languages: ["Java"],
     year: 2019,
@@ -82,10 +91,12 @@ export const projects = [
       "The card game Hearts, with all standard rules, for 1 up to 52 human and/or computer players. One computer player programmed. Text-based interface, only setup for 1 human player and any number of computer players.",
   },
   {
+    id: 5,
     title: "Blindfolded Cubing System",
-    id: 201903,
-    url: "https://docs.google.com/spreadsheets/d/1Ml7Jl5Yeb9HeE14xO7j1Nc99sw08oiBjLbi_nYJk9rU/edit?usp=sharing",
     image: blindfoldedSystemImage,
+    type: "Personal",
+    // icon: , // !!!
+    url: "https://docs.google.com/spreadsheets/d/1Ml7Jl5Yeb9HeE14xO7j1Nc99sw08oiBjLbi_nYJk9rU/edit?usp=sharing",
     tags: ["System", "Rubik's Cubes"],
     languages: [],
     year: 2019,
@@ -93,10 +104,12 @@ export const projects = [
       "My personal system, both memorization and execution, for solving various sizes of Rubik's cubes blindfolded. Also extended for memory sports, including memorization of numbers and decks of cards.",
   },
   {
+    id: 6,
     title: "3-Style Corner Alg Generator",
-    id: 201904,
-    url: "https://csclub.uwaterloo.ca/~krmatthe/3-style-corners.cgi",
     image: threeStyleCornerAlgGeneratorImage,
+    type: "Personal",
+    // icon: , // !!!
+    url: "https://csclub.uwaterloo.ca/~krmatthe/3-style-corners.cgi",
     tags: ["Web App", "Rubik's Cubes"],
     languages: ["Racket"],
     year: 2019,
@@ -104,10 +117,12 @@ export const projects = [
       "Given 3 cyclicly-permuted corners of a Rubik's cube, finds (intuitive) commutators which solve the case and sorts them by estimated speed (based on move-count, finger-friendliness).",
   },
   {
+    id: 7,
     title: "Corner Twist Alg Generator",
-    id: 202001,
-    url: "https://csclub.uwaterloo.ca/~krmatthe/corner-twist.cgi",
     image: cornerTwistAlgGeneratorImage,
+    type: "Personal",
+    // icon: , // !!!
+    url: "https://csclub.uwaterloo.ca/~krmatthe/corner-twist.cgi",
     tags: ["Web App", "Rubik's Cubes"],
     languages: ["Racket"],
     year: 2020,
@@ -115,11 +130,14 @@ export const projects = [
       "Given 2 or 3 twisted corners of a Rubik's cube, finds pairs of (intuitive) commutators which solve the case and sorts them by estimated speed (based on move-count, finger-friendliness).",
   },
   {
+    id: 8,
     title: "Personal Website",
-    id: 202101,
-    // use github repo since are already on the site
-    url: "https://github.com/kr-matthews/kr-matthews.github.io",
     image: personalWebsiteImage,
+    type: "Personal",
+    icon: personalWebsiteIcon,
+    url: "https://kr-matthews.github.io/",
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/kr-matthews.github.io",
     tags: ["Web App", "Website"],
     languages: ["React"],
     year: 2021,
@@ -127,10 +145,14 @@ export const projects = [
       "Custom-built website using React and CSS, including project gallery, blog, and more.",
   },
   {
+    id: 9,
     title: "Tic Tac Toe",
-    id: 202102,
-    url: "https://kr-matthews.github.io/tic-tac-toe",
     image: ticTacToeImage,
+    type: "Personal",
+    // icon: , // !!!
+    url: "https://kr-matthews.github.io/tic-tac-toe",
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/tic-tac-toe",
     tags: ["Web App", "Game"],
     languages: ["React"],
     year: 2021,
@@ -138,10 +160,14 @@ export const projects = [
       "There are two players playing against each other on a common interface (screen). Each player can be human-controlled or one of several built-in computer players (of varying difficulty).",
   },
   {
+    id: 10,
     title: "Minesweeper",
-    id: 202103,
-    url: "https://kr-matthews.github.io/minesweeper",
     image: minesweeperImage,
+    type: "Personal",
+    // icon: , // !!!
+    url: "https://kr-matthews.github.io/minesweeper",
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/minesweeper",
     tags: ["Web App", "Game"],
     languages: ["React"],
     year: 2021,
@@ -149,10 +175,14 @@ export const projects = [
       "The standard minesweeper game for one human player. Select from standard difficulties, or specify custom parameters. Times and tracks high-scores.",
   },
   {
+    id: 11,
     title: "Connect 4",
-    id: 202104,
-    url: "https://kr-matthews.github.io/connect-4",
     image: connectFourImage,
+    type: "Personal",
+    // icon: , // !!!
+    url: "https://kr-matthews.github.io/connect-4",
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/connect-4",
     tags: ["Web App", "Game"],
     languages: ["React"],
     year: 2021,
@@ -160,10 +190,14 @@ export const projects = [
       "Play against the computer or a friend locally, or play remotely in a virtual room (via PubNub). Includes custom name and colour, and optional sound effects.",
   },
   {
+    id: 12,
     title: "Cribbage",
-    id: 202201,
-    url: "https://kr-matthews.github.io/cribbage",
     image: cribbageImage,
+    type: "Personal",
+    // icon: , // !!!
+    url: "https://kr-matthews.github.io/cribbage",
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/cribbage",
     tags: ["Web App", "Game", "Cards"],
     languages: ["React"],
     year: 2022,
@@ -171,10 +205,14 @@ export const projects = [
       "A game of Cribbage for 2 or 3 players, with remote play. Includes score board, match log, sounds effects, automatic scoring, computer players, and more.",
   },
   {
-    title: "Competition Current Groups",
-    id: 202202,
-    url: "https://kr-matthews.github.io/stage-groups",
+    id: 13,
+    title: "Competition Groups Display",
     image: stageGroupsImage,
+    type: "Volunteer",
+    // icon: "", // !!!
+    url: "https://kr-matthews.github.io/stage-groups",
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/stage-groups",
     tags: ["Web App", "Rubik's Cubes"],
     languages: ["React"],
     year: 2022,
@@ -182,10 +220,14 @@ export const projects = [
       "A quick and simple web app to project in the main room at the North American Championships 2022 Rubik's Cube competition.",
   },
   {
+    id: 14,
     title: "Activities On-This-Day",
-    id: 202203,
-    url: "https://activities-on-this-day.netlify.app/",
     image: activitiesOnThisDayImage,
+    type: "Personal",
+    icon: activitiesOnThisDayIcon,
+    url: "https://activities-on-this-day.netlify.app/",
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/activities-on-this-day",
     tags: ["Web App", "Strava", "API"],
     languages: ["React"],
     year: 2022,
@@ -193,22 +235,44 @@ export const projects = [
       "An app to display all your Strava activities from this day in history, using Netlify 'serverless functions' and the Strava API.",
   },
   {
+    id: 15,
     title: "Heavy Footer with Links",
-    id: 202301,
-    url: "https://github.com/kr-matthews/footer-dependency",
     image: footerImage,
+    type: "Personal",
+    icon: personalWebsiteIcon,
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/footer-dependency",
     tags: ["Dependency"],
     languages: ["React"],
     year: 2023,
     description:
       "Essentially an unpublished npm package with a custom footer component (plus links) to reuse across all my web apps.",
   },
+  {
+    id: 16,
+    title: "Website Refactor",
+    image: "", // !!
+    type: "Personal",
+    icon: personalWebsiteIcon,
+    url: "https://kr-matthews.github.io/",
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/kr-matthews.github.io",
+    tags: ["Web App", "Website"],
+    languages: ["React"],
+    year: 2023,
+    description:
+      "Update to React 18 and React Router 6. Add Chakra UI and replace all styling. Improve project structure and best practices, including separation of concerns and reusability.",
+  },
   /*
   {
+    id: ,
     title: "",
-    id: 2023xy,
-    url: "",
     image: "",
+    type: "",
+    icon: "",
+    url: "https://kr-matthews.github.io/...",
+    codeHost: "GitHub",
+    codeUrl: "https://github.com/kr-matthews/...",
     tags: [],
     languages: [],
     year: 2023,
@@ -216,3 +280,5 @@ export const projects = [
   }
   */
 ];
+
+// !!! figure out ids, and sorting
