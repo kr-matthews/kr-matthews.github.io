867b75036aa457000916a89c2bc64ed244e901b9
Kevin Matthews <kvnrmatthews@gmail.com>
2023-02-19 20:50:14 -0800

    Redo ArticlePreview UI; show more

--- src/components/pages/blog/ArticlePreview.jsx
+++ src/components/pages/blog/ArticlePreview.jsx
@@ -1,30 +1,47 @@
-import { Link } from "react-router-dom";
+import { Box, Center, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import notepad from "../../../assets/notepad.png";
+import Link from "../../common/Link";
 
 export default function ArticlePreview({
-  link = "",
-  title = "Article",
-  // startDate,
+  link,
+  title = "Untitled",
   publishDate,
-  // editDate,
-  image = notepad,
-  // tags = [],
+  editDate,
+  image,
+  tags = [],
   preview = "Click to read.",
 }) {
   return (
-    <Link to={link} className="item">
-      <div className="image-container">
-        <img src={image} alt={title} />
-      </div>
-      <div className="info-container">
-        <h2>{title}</h2>
-        <div className="specs">
-          {publishDate
-            ? publishDate.toISOString().split("T")[0]
-            : "Unknown Date"}
-        </div>
-        <div className="preview">{preview}</div>
-      </div>
+    <Link
+      to={link}
+      withoutUnderline
+      isDisabled={!link}
+      disabledTitle="Currently unavailable"
+    >
+      <HStack spacing="2em" h="16em" p="2em">
+        <Center h="100%" minW="13em">
+          <Image src={image || notepad} maxH="100%" maxW="100%" />
+        </Center>
+        <VStack width="100%">
+          <Text as="h2" m={0} noOfLines={2}>
+            {title}
+          </Text>
+          <HStack textStyle="specs">
+            <Box>
+              Published:{" "}
+              {publishDate
+                ? publishDate.toISOString().split("T")[0]
+                : "Unknown Date"}
+            </Box>
+            <Box>
+              {editDate &&
+                "(Updated: " + editDate.toISOString().split("T")[0] + ")"}
+            </Box>
+          </HStack>
+          {tags.length > 0 && <Box textStyle="specs">{tags.join(", ")}</Box>}
+          <Text noOfLines={4}>{preview}</Text>
+        </VStack>
+      </HStack>
     </Link>
   );
 }
