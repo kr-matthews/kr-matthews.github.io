d4bbc41b43b8b403d64cb8040babde831aa93e9e
Kevin Matthews <kvnrmatthews@gmail.com>
2023-03-17 09:02:06 -0700

    Add Toggle unit tests

--- src/components/common/Toggle.jsx
+++ src/components/common/Toggle.jsx
@@ -1,13 +1,20 @@
-import { HStack, Switch } from "@chakra-ui/react";
+import { FormControl, FormLabel, HStack, Switch, Text } from "@chakra-ui/react";
 import React from "react";
 
-export default function Toggle({ onToggle, left, right, ...props }) {
+export default function Toggle({ label, onToggle, left, right, ...props }) {
   const onChange = onToggle ? (e) => onToggle(e.target.checked) : undefined;
   return (
-    <HStack>
-      {left}
-      <Switch onChange={onChange} size="md" {...props} />
-      {right}
-    </HStack>
+    <FormControl>
+      {label && (
+        <FormLabel>
+          <Text>{label}</Text>
+        </FormLabel>
+      )}
+      <HStack>
+        {left}
+        <Switch onChange={onChange} size="md" {...props} />
+        {right}
+      </HStack>
+    </FormControl>
   );
 }
